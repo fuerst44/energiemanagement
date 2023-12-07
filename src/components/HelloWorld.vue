075dadfaedc91105425171c8308838c2acc6e0a4
Fabian313 <langba06@ads.uni-passau.de>
2023-12-07 06:57:32 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <img src="./imageStart.jpg"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+  <div class="header-container">
+    <div class="homeButton">
+      <button id="homeBtn">
+        <i class="fas fa-home"></i>
+      </button>
+    </div>
+    <div class="search-button">
+      <button>
+        <i class="fas fa-search"></i>
+      </button>
+    </div>
+ </div>
+  <div class="headerName">
+    <h1>Admin Startseite</h1>
+  </div>
+  <div class="body-container">
+    <button class ="button-large" id="accountManagementBtn">Kontoverwaltung</button>
+    <button class ="button-large" id="logBtn">LOG</button>
+  </div>
+  <div class="footer-container">
+    <button class ="button-large" id="logoutBtn">Logout</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+@import './style.css';
+</style>
+
+
